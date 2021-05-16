7200f0140a447f1e5a0248cdf9b4630da08d105b
a2ais <a2ais@a2aiss-mbp.lan>
2021-05-16 17:55:38 -0400

    updated parameters

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,5 @@
-def call(String name ,String dayOfWeek)
+def call(Map config= [:])
 { 
-"echo Hello World $name and Today is $dayOfWeek"
+"echo Hello World $(config.name) and Today is $(config.dayOfWeek)"
 
 }
