4f9a1c8fa08cc4e5cde79a3c8d0b3423684aa954
a2ais <a2ais@a2aiss-mbp.lan>
2021-05-16 18:27:02 -0400

    updated parameters

---
+++ vars/devDeploy.groovy
@@ -0,0 +1,21 @@
+def call(Map config= [:])
+{ 
+	
+	envVariables = [
+						"cloneURL" : ${config.cURL)},
+						"cloneBranch" : ${config.cBranch},
+      					"email" : ${config.email}
+					]
+	deployProperties = [
+
+					"businessGroupName" : ${config.bgName},
+					"targetEnvironmentName" : ${config.teName},
+					"numberOfWorkers" : ${config.nofWorkers},
+					"workerSize" : ${config.wSize},
+					"deployRegion" : ${config.dRegion},
+					"deployNamePrefix" : ${config.dnPrefix},
+					"muleKeyId" : ${config.mkId}
+				]
+
+	sh "echo envVariables and config variables initialized!"
+}
