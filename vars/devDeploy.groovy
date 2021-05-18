b45373c67d6cc4ade3b6cbed3153cfb6d64c773b
ram <rkvajrala@gmail.com>
2021-05-18 17:51:45 -0400

    updated code

--- vars/devDeploy.groovy
+++ vars/devDeploy.groovy
@@ -60,7 +60,7 @@ def call(Map config= [:])
 		}
 		stage('Get Application Properties') 
 		{
-			def configPath = 'src/main/resources/' + deployProperties.targetEnvironmentName +'.yaml'
+			//def configPath = 'src/main/resources/' + deployProperties.targetEnvironmentName +'.yaml'
 			//def yamlProps = readYaml file: 'src/main/resources/' + deployProperties.targetEnvironmentName +'.yaml'
 			//def jsonProps = JsonOutput.toJson(yamlProps)
 			//def parsedJson = new JsonSlurperClassic().parseText(jsonProps)
@@ -68,8 +68,8 @@ def call(Map config= [:])
 						
 			//sh'''
 			//				rm -r $WORKSPACE/external-properties
-			'''''
-			sh "echo config properties : ${configPath}"
+			//'''''
+			sh "echo config properties : ${WORKSPACE}"
 			println("== Sucessfully Read the properties from location external-properties/config-" + deployProperties.targetEnvironmentName + ".yaml ==")
 				
 		}
